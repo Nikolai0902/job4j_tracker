dd6512961f7d3591c557e5195d9ecd1b07c78e4d
Nikolai0902 <kbus94@yandex.ru>
2021-02-28 13:13:45 +0300

    1.7. Конструктор [#453415]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desc;
+
+    public Fix(String one) {
+        this.desc = one;
+    }
+
+    public static void main(String[] args) {
+        Fix item = new Fix("1");
+    }
+}
