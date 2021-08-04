ac02a8864dacd1d463bd21be7ee684ffe3e009d9
Nikolai0902 <k1o2l3i49494>
2021-08-04 12:46:22 +0300

    4.2. Статические методы. [#181779]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -35,14 +35,12 @@ public class StartUI {
     }
 
     public static void delete(Input input, Tracker tracker) {
-        System.out.println("=== Edit item ====");
+        System.out.println("=== Delete item ====");
         int id = input.askInt("Enter id: ");
-        String name = input.askStr("Enter name:");
-        Item item = new Item(name);
-        if (tracker.replace(id, item)) {
-            System.out.println("Заявка изменена успешно.");
+        if (tracker.delete(id)) {
+            System.out.println("Заявка удалена успешно.");
         } else {
-            System.out.println("Ошибка замены заявки.");
+            System.out.println("Ошибка удаления заявки.");
         }
     }
 
