9849424e6c975d24df7524887285ec5a05be3d7a
Nikolai0902 <k1o2l3i49494>
2021-09-15 10:27:11 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList names = new ArrayList<String>();
+        names.add("Petr");
+        names.add("Ivan");
+        names.add("Stepan");
+        for (Object n: names) {
+            System.out.println(n);
+        }
+    }
+}
