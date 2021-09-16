383ca3373cbf814484795632838a3e0ee1e6ba97
Nikolai0902 <k1o2l3i49494>
2021-09-16 09:45:04 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #203580]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -3,8 +3,10 @@ package ru.job4j.search;
 import org.junit.Test;
 
 import java.util.ArrayList;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertNotNull;
 
 public class PhoneDictionaryTest {
 
@@ -16,5 +18,15 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByNameNull() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Niko");
+        assertNotNull(persons);
     }
 }
