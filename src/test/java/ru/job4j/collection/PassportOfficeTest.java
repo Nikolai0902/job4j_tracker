c7a83a055a7eb1f4b9fc9baf7f5334f4647aa258
Nikolai0902 <k1o2l3i49494>
2021-09-21 11:13:41 +0300

    1. Паспорт и жители [#212656 #205728]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -3,6 +3,7 @@ package ru.job4j.collection;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.*;
 
 public class PassportOfficeTest {
@@ -13,5 +14,14 @@ public class PassportOfficeTest {
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void notAdd() {
+        Citizen citizenOne = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizenTwo = new Citizen("2f44a", "Alex Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizenOne);
+        assertFalse(office.add(citizenTwo));
     }
 }
