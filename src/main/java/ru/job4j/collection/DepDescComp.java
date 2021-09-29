dedb34fa38a2f7a8a70614b78b9a059b87e4f969
Nikolai0902 <k1o2l3i49494>
2021-09-29 08:38:48 +0300

    2. Отсортировать департаменты [#34352 #209692]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,12 +7,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] ob1 = o1.split("/");
         String[] ob2 = o2.split("/");
-        int rsl;
-        if (ob1[0].equals(ob2[0])) {
-            rsl = o1.compareTo(o2);
-        } else {
-            rsl = o2.compareTo(o1);
-        }
-        return rsl;
+        int rsl = ob2[0].compareTo(ob1[0]);
+        return rsl != 0 ? rsl : o1.compareTo(o2);
     }
 }
